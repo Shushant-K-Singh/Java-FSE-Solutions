ddc8e0416ad3084dc0ef8c98306aaa28bc69b04f
Shushant-K-Singh <shushantkumar860@gmail.com>
2026-07-09 23:06:15 +0530

    Mandatory Hands-on completed based on Spring Core and Maven

---
+++ Spring_Core_And_Maven/Mandatory/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,11 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    public void showBooks() {
+        System.out.println("Inside BookService");
+    }
+
+}
